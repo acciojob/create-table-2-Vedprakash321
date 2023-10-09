c58df80dfcd4094755ff6af95651802639394fac
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-10-10 04:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-// function createTable() {
-//     //Write your code here
-	
-  
-// }
 function createTable() {
-    // Prompt for the number of rows
-    const numRows = parseInt(prompt("Input number of rows"));
-    
-    // Prompt for the number of columns
-    const numCols = parseInt(prompt("Input number of columns"));
-    
-    // Get the table element by its ID
-    const table = document.getElementById("myTable");
-    
-    // Clear the table content if it already has any
-    table.innerHTML = '';
-    
-    // Create rows and columns based on user input
-    for (let i = 0; i < numRows; i++) {
-        // Create a new row
-        const row = table.insertRow(i);
-        
-        for (let j = 0; j < numCols; j++) {
-            // Create a new cell in the row
-            const cell = row.insertCell(j);
-            
-            // Set the text content of the cell
-            cell.textContent = `Row-${i} Column-${j}`;
-        }
-    }
-}
+    // Prompt the user for the number of rows and columns
+    const rows = parseInt(prompt("Input number of rows"));
+    const columns = parseInt(prompt("Input number of columns"));
 
-// Get the button element by its ID and attach click event listener
-const createButton = document.getElementById("createButton");
-createButton.addEventListener("click", createTable);
+    // Get the table element from the DOM
+    const table = document.getElementById('myTable');
+
+    // Clear any existing content in the table
+    table.innerHTML = '';
+
+    // Loop through rows and columns to create cells with content
+    for (let i = 0; i < rows; i++) {
+        // Create a table row element
+        const row = document.createElement('tr');
+
+        for (let j = 0; j < columns; j++) {
+            // Create a table cell element
+            const cell = document.createElement('td');
+
+            // Set the content of the cell
+            cell.textContent = `Row-${i} Column-${j}`;
+
+            // Append the cell to the row
+            row.appendChild(cell);
+        }
+
+        // Append the row to the table
+        table.appendChild(row);
+    }
+
+    // Display prompt text
+    // alert(`Table created with ${rows} rows and ${columns} columns.`);
+}
